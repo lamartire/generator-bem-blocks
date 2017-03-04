414802b268963989b9ced074bbd2bea132d942a5
lamartire <la_martire@mail.ru>
2017-03-04 23:31:52 +0300

    Common tests

--- test/app.js
+++ test/app.js
@@ -3,16 +3,45 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
-describe('generator-bem-blocks:app', function () {
+describe('generator-bem-blocks:pug+styl (common generation)', function () {
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
+      .withPrompts({
+        blockPath: 'test',
+        blockName: 'helloWorld',
+        markup: 'pug',
+        style: 'styl'
+      })
       .toPromise();
   });
 
-  it('creates files', function () {
+  it('creates directory helloWorld with helloWorld.pug and helloWorld.styl files', function () {
     assert.file([
-      'dummyfile.txt'
+      'test/helloWorld',
+      'test/helloWorld/helloWorld.pug',
+      'test/helloWorld/helloWorld.styl'
+    ]);
+  });
+});
+
+describe('generator-bem-blocks:jsx+styl (common generation)', function () {
+  before(function () {
+    return helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts({
+        blockPath: 'test',
+        blockName: 'helloWorld',
+        markup: 'jsx',
+        style: 'styl'
+      })
+      .toPromise();
+  });
+
+  it('creates directory helloWorld with helloWorld.jsx, helloWorld.styl and index.jsx files', function () {
+    assert.file([
+      'test/helloWorld',
+      'test/helloWorld/index.jsx',
+      'test/helloWorld/helloWorld.jsx',
+      'test/helloWorld/helloWorld.styl'
     ]);
   });
 });
